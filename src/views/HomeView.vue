709dff5510cbfc5ebcc7573744524ba4018edb55
webdevyoo <yjk7454@naver.com>
2024-12-23 01:47:32 +0900

    feat: `007 vue 맛보기` - v-디렉티브 및 데이터 변수 맛보기 [v-bind, v-if, v-for]

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <span v-bind:title="title">
+      {{ message }}
+    </span>
+    <p v-if="seen">
+      이제 나를 볼 수 있어요!
+    </p>
+    <v-alert type="primary" :value="true" v-for="value in 5" :key="value">
+      abcde {{ value }}
+    </v-alert>
+    <v-alert type="primary" :value="true" v-for="(value, index) in array" :key="index">
+      abcde {{ value }}
+    </v-alert>
   </div>
 </template>
 
@@ -13,6 +23,14 @@ export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      seen: true,
+      message: '내 위에 잠시마 마우스를 올리면 동적으로 바인딩 된 title을 볼 수 있다.',
+      title: '이 페이지는 ' + new Date() + ' 에 로드 되었습니다.',
+      array: [1, 2, 3, 4, 'www', 'xxx']
+    }
   }
 }
 </script>
