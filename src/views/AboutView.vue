5556dd4254214ec74f38c5deeefda43084d5995e
webdevyoo <yjk7454@naver.com>
2024-12-23 21:36:04 +0900

    feat: `009 vuetify로 리스트 만들기` - about.vue 컴포넌트 템플릿 레이아웃 구현

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-list two-line>
+    <v-list-tile>
+      <v-list-tile-avatar>
+        <v-icon>mdi-alert</v-icon>
+      </v-list-tile-avatar>
+      <v-list-tile-content>
+        <v-list-tile-title>Tile Title</v-list-tile-title>
+        <v-list-tile-sub-title>Tile Sub Title</v-list-tile-sub-title>
+      </v-list-tile-content>
+      <v-list-tile-action>
+        <v-btn icon ripple>
+          <v-icon color="grey lighten-1">mdi-alert-box</v-icon>
+        </v-btn>
+      </v-list-tile-action>
+    </v-list-tile>
+  </v-list>
 </template>
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
