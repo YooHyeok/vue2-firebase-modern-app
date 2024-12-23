99bca6c4d24bec69d6e88c3dd62501f55d98fc9d
webdevyoo <yjk7454@naver.com>
2024-12-24 05:15:09 +0900

    feat: `009 vuetify로 리스트 만들기` - about.vue data변수 구성 및 v-for 디렉티브 적용

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,16 @@
 <template>
   <v-list two-line>
-    <v-list-tile>
+    <v-list-tile v-for="item in items" :key="item.title">
       <v-list-tile-avatar>
-        <v-icon>mdi-alert</v-icon>
+        <v-icon>{{item.icon}}</v-icon>
       </v-list-tile-avatar>
       <v-list-tile-content>
-        <v-list-tile-title>Tile Title</v-list-tile-title>
-        <v-list-tile-sub-title>Tile Sub Title</v-list-tile-sub-title>
+        <v-list-tile-title>Tile Title - {{ item.title }}</v-list-tile-title>
+        <v-list-tile-sub-title>Tile Sub Title - {{ item.title }}</v-list-tile-sub-title>
       </v-list-tile-content>
       <v-list-tile-action>
         <v-btn icon ripple>
-          <v-icon color="grey lighten-1">mdi-alert-box</v-icon>
+          <v-icon color="grey lighten-1">{{item.icon}}</v-icon>
         </v-btn>
       </v-list-tile-action>
     </v-list-tile>
@@ -20,7 +20,11 @@
 export default {
   data () {
     return {
-
+      items: [
+        { title: 'title1', icon: 'mdi-alert' },
+        { title: 'title2', icon: 'mdi-alert-box' },
+        { title: 'title3', icon: 'mdi-alert-circle' }
+      ]
     }
   }
 }
