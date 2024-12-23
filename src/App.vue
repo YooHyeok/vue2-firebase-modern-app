de07db05f6fd46cd3939b5081b0c4f11b2148fbf
webdevyoo <yjk7454@naver.com>
2024-12-24 05:19:00 +0900

    feat: `010 vuetify로 네비게이션 드라워 만들기` - 009 리스트 코드 재활용 적용

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
   <v-app>
     <v-navigation-drawer v-model="drawer" fixed>
-      abcde
+      <v-list two-line>
+        <v-list-tile v-for="item in items" :key="item.title">
+          <v-list-tile-avatar>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>Tile Title - {{ item.title }}</v-list-tile-title>
+            <v-list-tile-sub-title>Tile Sub Title - {{ item.title }}</v-list-tile-sub-title>
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-btn icon ripple>
+              <v-icon color="grey lighten-1">{{item.icon}}</v-icon>
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
     <v-toolbar color="lime">
       <v-toolbar-side-icon @click="test"/> <!-- 햄버거 아이콘 -->
@@ -33,7 +48,12 @@ export default {
   },
   data () {
     return {
-      drawer: false
+      drawer: false,
+      items: [
+        { title: 'title1', icon: 'mdi-alert' },
+        { title: 'title2', icon: 'mdi-alert-box' },
+        { title: 'title3', icon: 'mdi-alert-circle' }
+      ]
     }
   },
   methods: {
