7d51c62b09879f24cfb0841392f4c8a73258b56b
webdevyoo <yjk7454@naver.com>
2024-12-27 00:10:52 +0900

    feat: `015 vuetify 노트 만들기` - Notes.vue vuetify v-data-iterator 기본 예제 적용

--- src/views/lectures/Notes.vue
+++ src/views/lectures/Notes.vue
@@ -1,6 +1,16 @@
 <template>
   <v-container fluid grid-list-md>
-    <!-- v-layout의 경우 Vuetify VLayout 컴포넌트로 전역컴포넌트 등록을 해준다. -->
+    <v-layout> <!-- v-layout의 경우 Vuetify VLayout 컴포넌트로 전역컴포넌트 등록을 해준다. -->
+      <v-flex xs5>
+        <v-text-field v-model="title" label="title"></v-text-field>
+      </v-flex>
+      <v-flex xs5>
+        <v-text-field v-model="content" label="content"></v-text-field>
+      </v-flex>
+      <v-flex xs2>
+        <v-btn color="success" @click="post">post</v-btn>
+      </v-flex>
+    </v-layout>
     <v-data-iterator
       :items="items"
       :rows-per-page-items="rowsPerPageItems"
@@ -8,42 +18,15 @@
       content-tag="v-layout"
       row
       wrap
-    >
-      <template v-slot:item="props">
-        <v-flex xs12 sm md4 lg3>
-          <v-card>
-            <v-card-title><h4>{{ props.item.name }}</h4></v-card-title>
+    > <!-- pagenation의 경우 rowsPerPage, descending, page, sortBy, totalItems 등의 속성들이 자동으로 할당된다. -->
+    <template v-slot:item="props">
+      <v-flex xs12 sm md4 lg3>
+        <v-card>
+            <v-card-title><h4>{{ props.item.title }}</h4></v-card-title>
             <v-divider></v-divider>
-            <v-list dense>
-              <v-list-tile>
-                <v-list-tile-content>Categories:</v-list-tile-content>
-                <v-list-tile-content class="align-end">{{ props.item.calories }}</v-list-tile-content>
-              </v-list-tile>
-              <v-list-tile>
-                <v-list-tile-content>Fat:</v-list-tile-content>
-                <v-list-tile-content class="align-end">{{ props.item.fat }}</v-list-tile-content>
-              </v-list-tile>
-              <v-list-tile>
-                <v-list-tile-content>Carbs:</v-list-tile-content>
-                <v-list-tile-content class="align-end">{{ props.item.carbs }}</v-list-tile-content>
-              </v-list-tile>
-              <v-list-tile>
-                <v-list-tile-content>Protein:</v-list-tile-content>
-                <v-list-tile-content class="align-end">{{ props.item.protein }}</v-list-tile-content>
-              </v-list-tile>
-              <v-list-tile>
-                <v-list-tile-content>Sodium:</v-list-tile-content>
-                <v-list-tile-content class="align-end">{{ props.item.sodium }}</v-list-tile-content>
-              </v-list-tile>
-              <v-list-tile>
-                <v-list-tile-content>Calcium:</v-list-tile-content>
-                <v-list-tile-content class="align-end">{{ props.item.calcium }}</v-list-tile-content>
-              </v-list-tile>
-              <v-list-tile>
-                <v-list-tile-content>Iron:</v-list-tile-content>
-                <v-list-tile-content class="align-end">{{ props.item.iron }}</v-list-tile-content>
-              </v-list-tile>
-            </v-list>
+            <v-card-text>
+              {{ props.item.content }}
+            </v-card-text>
           </v-card>
         </v-flex>
       </template>
@@ -57,108 +40,30 @@ export default {
     pagination: {
       rowsPerPage: 4
     },
-    items: [
-      {
-        name: 'Frozen Yogurt',
-        calories: 159,
-        fat: 6.0,
-        carbs: 24,
-        protein: 4.0,
-        sodium: 87,
-        calcium: '14%',
-        iron: '1%'
-      },
-      {
-        name: 'Ice cream sandwich',
-        calories: 237,
-        fat: 9.0,
-        carbs: 37,
-        protein: 4.3,
-        sodium: 129,
-        calcium: '8%',
-        iron: '1%'
-      },
-      {
-        name: 'Eclair',
-        calories: 262,
-        fat: 16.0,
-        carbs: 23,
-        protein: 6.0,
-        sodium: 337,
-        calcium: '6%',
-        iron: '7%'
-      },
-      {
-        name: 'Cupcake',
-        calories: 305,
-        fat: 3.7,
-        carbs: 67,
-        protein: 4.3,
-        sodium: 413,
-        calcium: '3%',
-        iron: '8%'
-      },
-      {
-        name: 'Gingerbread',
-        calories: 356,
-        fat: 16.0,
-        carbs: 49,
-        protein: 3.9,
-        sodium: 327,
-        calcium: '7%',
-        iron: '16%'
-      },
-      {
-        name: 'Jelly bean',
-        calories: 375,
-        fat: 0.0,
-        carbs: 94,
-        protein: 0.0,
-        sodium: 50,
-        calcium: '0%',
-        iron: '0%'
-      },
-      {
-        name: 'Lollipop',
-        calories: 392,
-        fat: 0.2,
-        carbs: 98,
-        protein: 0,
-        sodium: 38,
-        calcium: '0%',
-        iron: '2%'
-      },
-      {
-        name: 'Honeycomb',
-        calories: 408,
-        fat: 3.2,
-        carbs: 87,
-        protein: 6.5,
-        sodium: 562,
-        calcium: '0%',
-        iron: '45%'
-      },
-      {
-        name: 'Donut',
-        calories: 452,
-        fat: 25.0,
-        carbs: 51,
-        protein: 4.9,
-        sodium: 326,
-        calcium: '2%',
-        iron: '22%'
-      },
-      {
-        name: 'KitKat',
-        calories: 518,
-        fat: 26.0,
-        carbs: 65,
-        protein: 7,
-        sodium: 54,
-        calcium: '12%',
-        iron: '6%'
-      }
-    ]
-  })
+    items: [],
+    title: '',
+    content: ''
+  }),
+  mounted () {
+
+  },
+  methods: {
+    post () {
+      this.items.push({
+        title: this.title, content: this.content
+      })
+      this.title = ''
+      this.content = ''
+    },
+    get () {
+
+    },
+    update () {
+
+    },
+    delete () {
+
+    }
+  }
 }
 </script>
